aa3ddcdfc8e18be2e671f222333ca5fe70531487
SogaN <98285469+SogaNN@users.noreply.github.com>
2022-01-27 18:02:22 +0100

    Update server.lua
    Optymalizacja kodu

--- sogan_nagrody/server/server.lua
+++ sogan_nagrody/server/server.lua
@@ -2,9 +2,8 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterNetEvent('sogan:nyggagnugagnuangunagungunau')
-AddEventHandler('sogan:nyggagnugagnuangunagungunau', function()
+RegisterNetEvent('sogan:addmoneyminuts')
+AddEventHandler('sogan:addmoneyminuts', function()
     local xPlayer = ESX.GetPlayerFromId(source)
     xPlayer.addMoney(Config.Cena)
-    print("Gracz Odebrał Nagrodę za 30 minut rozgrywki!")
 end)
